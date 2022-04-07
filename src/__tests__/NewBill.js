0b0cdfeaabfcff57392c71c2f8b6a776873ba822
RpRun <82530937+RpRun@users.noreply.github.com>
2022-04-07 18:03:25 +0200

    working on mockStore and NewBill POST

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -10,7 +10,11 @@ import { localStorageMock } from "../__mocks__/localStorage.js"
 import router from "../app/Router.js";
 import { ROUTES_PATH } from "../constants/routes.js";
 
+import bills from "../__mocks__/store.js"
+import mockStore from "../__mocks__/store"
+
 window.alert = jest.fn();
+// jest.mock("../app/store", () => mockStore)
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
@@ -36,14 +40,14 @@ describe("Given I am connected as an employee", () => {
       expect(mailIcon.classList.contains('active-icon')).toBeTruthy()
       // expect (newBillButton).toBeTruthy()
       // expect (newBillText).toBeTruthy()
-
+      // expect(screen.getAllByText('Envoyer une note de frais')).toBeTruthy()
 
     })
   
   })
 
   describe("When I am on NewBill Page", () => {
-    test("Then the message'Envoyer une note de frais' should be displayed", async () => {
+    test("Then the message 'Envoyer une note de frais' should be displayed", () => {
   
       const html = NewBillUI()
       document.body.innerHTML = html
@@ -67,7 +71,11 @@ describe("Given I am connected as an employee", () => {
       //*********************************************** */
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })   
       window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }))
+
       const NewBillController = new NewBill( { document, onNavigate, store: null, localStorage: window.localStorage } )
+
+      // const NewBillController = new NewBill( { document, onNavigate, store: mockStore, localStorage: window.localStorage } )
+
       // ======================================
       // error message
       const errorMessage = jest.fn(window.alert('Invalid file type'));  
@@ -75,62 +83,40 @@ describe("Given I am connected as an employee", () => {
       // input file
       const file = new File(['dummy content'], 'example.jpg', {type: 'image/jpg'})
       const handleChangeFile  = jest.fn(NewBillController.handleChangeFile)
-         
-      const inputFile = screen.getByTestId('file')
-      Object.defineProperty(inputFile, 'files', { value: [file] })
-
-      inputFile.addEventListener("change", handleChangeFile)
-      fireEvent.change(inputFile)
-      //********************************************************* */
-
-      
-
-
-      //********************************************************* */
-      expect(handleChangeFile).toHaveBeenCalled()     
-      expect(inputFile.files[0].name).toBe('example.jpg')      
-      expect(errorMessage).not.toHaveBeenCalled();
-      expect(errorMessage).not.toHaveBeenCalledWith('Invalid file type');
-      
-    })
-    test("then new data should be created", () => {
-      
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //*********************************************** */
-      const onNavigate = (pathname) => { document.body.innerHTML = ROUTES( { pathname } )}     
-      //*********************************************** */
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })   
-      window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }))
-      const NewBillController = new NewBill( { document, onNavigate, store: null, localStorage: window.localStorage } )
-      // ======================================
-      // error message
-      const errorMessage = jest.fn(window.alert('Invalid file type'));  
-      // ======================================
-      // input file
-      const file = new File(['dummy content'], 'example.jpg', {type: 'image/jpg'})
-      const handleChangeFile  = jest.fn(NewBillController.handleChangeFile)
-         
-      const inputFile = screen.getByTestId('file')
-      Object.defineProperty(inputFile, 'files', { value: [file] })
-
-      inputFile.addEventListener("change", handleChangeFile)
-      fireEvent.change(inputFile)
-      //********************************************************* */
-
-      
-
-
-      //********************************************************* */
-      expect(handleChangeFile).toHaveBeenCalled()     
-      expect(inputFile.files[0].name).toBe('example.jpg')      
-      expect(errorMessage).not.toHaveBeenCalled();
-      expect(errorMessage).not.toHaveBeenCalledWith('Invalid file type');
-      
-    })
   
+      const inputFile = screen.getByTestId('file')
+      Object.defineProperty(inputFile, 'files', { value: [file] })
+
+      inputFile.addEventListener("change", handleChangeFile)
+      fireEvent.change(inputFile)
+
+      
+
+      //********************************************************* */
+      // const testBills = jest.fn(bills)
+
+      // console.log(bills.length)
+      // console.log(testBills)
+      // console.log(bills.fileName)
+      // console.log(NewBillController.billId)
+
+      // expect(testBills).toHaveBeenCalled(); 
+
+      // expect(NewBillController.billId).not.toBe('') 
+      // expect(NewBillController.fileUrl).toBe(' ?')
+      // expect(NewBillController.fileName).toBe('example.jpg')  
+
+      //********************************************************* */
+
+      expect(handleChangeFile).toHaveBeenCalled() 
+      expect(inputFile.files[0].name).toBe('example.jpg')      
+      expect(errorMessage).not.toHaveBeenCalled();
+      expect(errorMessage).not.toHaveBeenCalledWith('Invalid file type');
+     
+    })
+
   })
-  
+
   describe('When I provide a non-picture as proof of invoice', () => {
     test("then an error-message should appear ", () => {
       
@@ -187,7 +173,9 @@ describe("Given I am connected as an employee", () => {
       document.body.innerHTML = html
     
       const onNavigate = (pathname) => { document.body.innerHTML = ROUTES({ pathname })} 
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })  
+      // localStorageMock necessaire?
+      //*************************************************************************** */
+      // Object.defineProperty(window, 'localStorage', { value: localStorageMock })  
       window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }))
 
       const NewBillController = new NewBill({ document, onNavigate, store: null, localStorage: window.localStorage })
@@ -209,7 +197,72 @@ describe("Given I am connected as an employee", () => {
 
 })
 
+// test d'intégration POST
+describe("Given I am a user connected as Employee", () => {
+  describe("When I send a new Bill", () => {
+    test("fetches bills from mock API POST", async () => {
+      localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "a@a" }));
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+     
+    
+    })
+       
+      describe("When an error occurs on API", () => {
+        beforeEach(() => {
+          jest.spyOn(mockStore, "bills")
+          Object.defineProperty(
+              window,
+              'localStorage',
+              { value: localStorageMock }
+          )
+          window.localStorage.setItem('user', JSON.stringify({
+            type: 'Employee',
+            email: "a@a"
+          }))
+          const root = document.createElement("div")
+          root.setAttribute("id", "root")
+          document.body.appendChild(root)
+          router()
 
+          
+        })
+
+        // console.log(mockStore)
+
+        test("fetches bills from an API and fails with 404 message error", async () => {
+          // console.log(mockStore)
+          // mockStore.bills.mockImplementationOnce(() => {
+          //   return {
+          //     list : () =>  {
+          //       return Promise.reject(new Error("Erreur 404"))
+          //     }
+          //   }})
+          // window.onNavigate(ROUTES_PATH.Bills)
+          // await new Promise(process.nextTick);
+          // const message = await screen.getByText(/Erreur 404/)
+          // expect(message).toBeTruthy()
+        })
+
+        // test("fetches messages from an API and fails with 500 message error", async () => {
+
+        //   mockStore.bills.mockImplementationOnce(() => {
+        //     return {
+        //       list : () =>  {
+        //         return Promise.reject(new Error("Erreur 500"))
+        //       }
+        //     }})
+
+        //   window.onNavigate(ROUTES_PATH.Bills)
+        //   await new Promise(process.nextTick);
+        //   const message = await screen.getByText(/Erreur 500/)
+        //   expect(message).toBeTruthy()
+        // })
+      })
+  })
+})
 
  
 
