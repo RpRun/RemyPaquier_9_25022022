8e3e70dedc9ce524e7f118b1f768311c86f0dfec
RpRun <82530937+RpRun@users.noreply.github.com>
2022-04-16 11:42:21 +0200

    handleEditTicket modification, dashboard  bug resolved when clicking on a bill

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -89,7 +89,7 @@ export default class {
 
     if (this.counter === undefined || this.id !== bill.id) this.counter = 0
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
-    if (this.counter % 1 === 0) {
+    if (this.counter % 2 === 0) {
       
       bills.forEach(b => {
 
